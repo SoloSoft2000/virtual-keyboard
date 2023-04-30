5677644255bbb3f815ab2be8ae15fdf5907dc5ed
Eugene Solomonyk <solosoft2000@mail.ru>
2023-04-30 13:57:10 +0300

    fix: change Keyboard for ES6

--- scipts/Keyboard.js
+++ scipts/Keyboard.js
@@ -43,11 +43,10 @@ const Keyboard = {
     ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight']],
   updateKeyboard: () => {
     const keyArr = [].concat(...Keyboard.keyList);
-    for (let iterator = 0; iterator < keyArr.length; iterator += 1) {
-      const keyChange = keyArr[iterator];
+    keyArr.forEach((keyChange) => {
       const domKey = document.querySelector(`#${keyChange}`);
       domKey.innerHTML = getKeyText(keyChange);
-    }
+    });
   },
   keyDown: (event) => {
     if (!event.repeat) {
@@ -130,33 +129,33 @@ const Keyboard = {
     domTextArea.focus();
   },
   create: (node) => {
-    for (let row = 0; row < Keyboard.keyList.length; row += 1) {
+    Keyboard.keyList.forEach((row, rowIdx) => {
       const newRow = document.createElement('div');
-      newRow.className = `row row${row}`;
-      for (let keyI = 0; keyI < Keyboard.keyList[row].length; keyI += 1) {
+      newRow.className = `row row${rowIdx}`;
+      row.forEach((keyName) => {
         const keyButton = document.createElement('button');
         keyButton.className = 'key__button';
-        keyButton.id = Keyboard.keyList[row][keyI];
-        if (keys[Keyboard.keyList[row][keyI]].service) {
-          if (keys[Keyboard.keyList[row][keyI]].double) {
+        keyButton.id = keyName;
+        if (keys[keyName].service) {
+          if (keys[keyName].double) {
             keyButton.className += ' key__double';
           }
-          if (Keyboard.keyList[row][keyI] === 'Space') {
+          if (keyName === 'Space') {
             keyButton.className += ' key__space';
             keyButton.innerHTML = ' ';
           } else {
-            keyButton.innerHTML = keys[Keyboard.keyList[row][keyI]].name;
+            keyButton.innerHTML = keyName.name;
           }
         } else {
-          keyButton.innerHTML = keys[Keyboard.keyList[row][keyI]][defaultLanguage.language];
+          keyButton.innerHTML = keyName[defaultLanguage.language];
         }
         keyButton.addEventListener('mousedown', Keyboard.keyDown);
         keyButton.addEventListener('mouseup', Keyboard.keyUp);
         keyButton.addEventListener('mouseout', Keyboard.mouseOut);
         newRow.append(keyButton);
-      }
+      });
       node.append(newRow);
-    }
+    });
   },
 };
 
